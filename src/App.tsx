7e9c2e30e01ebd08a254ce32266ff5ddc334f03f
Jozias Martini <jozias.martini@gmail.com>
2022-11-21 00:30:41 -0300

    Create the basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.sass";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="app">
+      <header className="app-header">
+        <h1 className="title">Coffee Shop</h1>
+        <p className="subtitle light-color">
+          Aberto todos os dias das 08h às 18h
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="subtitle light-color">Rua Machado de Assis, 397</p>
+
+        <section className="section-title">Cafés</section>
+
+        <section className="section-item">
+          <div className="text-left">
+            <span className="">Cappuccino</span>
+            <br />
+            <span className="font-small light-color">
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas
+            </span>
+          </div>
+          <div className="text-right">
+            <span className="">R$ 8,00</span>
+          </div>
+        </section>
+
+        <section className="section-item">
+          <div className="text-left">
+            <span className="">Café Expresso</span>
+            <br />
+            <span className="font-small light-color">
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas
+            </span>
+          </div>
+          <div className="text-right">
+            <span className="">R$ 8,00</span>
+          </div>
+        </section>
+
+        <section className="section-title">Lanches</section>
+
+        <section className="section-item">
+          <div className="text-left">
+            <span className="">Bolo de Laranja</span>
+            <br />
+            <span className="font-small light-color">
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas
+            </span>
+          </div>
+          <div className="text-right">
+            <span className="">R$ 8,00</span>
+          </div>
+        </section>
+
+        <section className="section-item">
+          <div className="text-left">
+            <span className="">Brownie</span>
+            <br />
+            <span className="font-small light-color">
+              Pellentesque habitant morbi tristique senectus et netus et
+              malesuada fames ac turpis egestas
+            </span>
+          </div>
+          <div className="text-right">
+            <span className="">R$ 8,00</span>
+          </div>
+        </section>
       </header>
     </div>
   );
