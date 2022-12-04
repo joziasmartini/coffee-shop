f5ee1efdfb464f85faeda8fe9986763b2df2279f
Jozias Martini <jozias.martini@gmail.com>
2022-12-04 16:16:21 -0300

    Add more products in the collection

--- src/utils/products.ts
+++ src/utils/products.ts
@@ -43,4 +43,42 @@ const coffees: Products[] = [
   },
 ];
 
-export { coffees };
+const snacks: Products[] = [
+  {
+    id: 1,
+    name: "Cookie",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+  {
+    id: 2,
+    name: "Brownie",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+  {
+    id: 3,
+    name: "Donut",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+  {
+    id: 4,
+    name: "Brigadeiro",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+  {
+    id: 5,
+    name: "Pudim",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+];
+
+export { coffees, snacks };
