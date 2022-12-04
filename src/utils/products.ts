3fd2c5356bbaa0957a7a4c241d216a36f830f6ce
Jozias Martini <jozias.martini@gmail.com>
2022-12-04 16:05:53 -0300

    Create product data in another file

---
+++ src/utils/products.ts
@@ -0,0 +1,46 @@
+type Products = {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+};
+
+const coffees: Products[] = [
+  {
+    id: 1,
+    name: "Cappuccino",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+  {
+    id: 2,
+    name: "Expresso",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+  {
+    id: 3,
+    name: "Macchiato",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+  {
+    id: 4,
+    name: "Latte",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+  {
+    id: 5,
+    name: "Mocha",
+    price: 8.0,
+    description:
+      "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+  },
+];
+
+export { coffees };
