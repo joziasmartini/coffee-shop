8d58d9d1807af56622894786ecef37c48d093a34
Jozias Martini <jozias.martini@gmail.com>
2022-12-04 16:16:36 -0300

    Use new products and footer on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.sass";
-import { coffees } from "./utils/products";
+import { coffees, snacks } from "./utils/products";
 
 function App() {
   return (
@@ -10,7 +10,8 @@ function App() {
           Aberto todos os dias das 08h às 18h
         </p>
         <p className="subtitle light-color">Rua Machado de Assis, 397</p>
-
+      </header>
+      <main>
         <section className="section-title">Cafés</section>
 
         {coffees.map((coffee) => (
@@ -30,34 +31,26 @@ function App() {
 
         <section className="section-title">Lanches</section>
 
-        <section className="section-item">
-          <div className="text-left">
-            <span className="">Bolo de Laranja</span>
-            <br />
-            <span className="font-small light-color">
-              Pellentesque habitant morbi tristique senectus et netus et
-              malesuada fames ac turpis egestas
-            </span>
-          </div>
-          <div className="text-right">
-            <span className="">R$ 8,00</span>
-          </div>
-        </section>
-
-        <section className="section-item">
-          <div className="text-left">
-            <span className="">Brownie</span>
-            <br />
-            <span className="font-small light-color">
-              Pellentesque habitant morbi tristique senectus et netus et
-              malesuada fames ac turpis egestas
-            </span>
-          </div>
-          <div className="text-right">
-            <span className="">R$ 8,00</span>
-          </div>
-        </section>
-      </header>
+        {snacks.map((snack) => (
+          <section className="section-item">
+            <div className="text-left">
+              <span className="">{snack.name}</span>
+              <br />
+              <span className="font-small light-color">
+                {snack.description}
+              </span>
+            </div>
+            <div className="text-right">
+              <span className="">R$ {snack.price}</span>
+            </div>
+          </section>
+        ))}
+      </main>
+      <p className="footer font-small">
+        ♡<br />
+        Faça seu pedido através do <br />
+        WhatsApp (99) 99999-9999
+      </p>
     </div>
   );
 }
